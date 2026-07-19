af143dffd9206e1f82226face8d362b9085b4cab
Subom Paul <subompaul9@gmail.com>
2026-07-19 12:52:34 +0530

    Add operations.js and move every operations to it

---
+++ scripts/operations.js
@@ -0,0 +1,102 @@
+import { noOfRepetitions } from './utils/string-utils.js';
+
+
+
+const equationElement = document.querySelector('.js-equation');
+
+
+export const operations = {
+    plus: function() {
+        if(!("/*-+%").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML += '+';
+        } else {
+            equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '+';
+        }
+    },
+
+    minus: function() {
+        if(!("/*-+%").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML += '-';
+        } else {
+            equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '-';
+        }
+    },
+
+    multiply: function() {
+        if((")%1234567890.").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML += '*';
+        } else if(equationElement.innerHTML.at(-1) === '(') {
+            return;
+        } else if (("/*-+").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '*';
+        }
+    },
+
+    divide: function() {
+        if((")%1234567890.").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML += '/';
+        } else if(equationElement.innerHTML.at(-1) === '(') {
+            return;
+        } else if (("/*-+").includes(equationElement.innerHTML.at(-1))) {
+            equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '/';
+        }
+    },
+
+    parenthesis: function() {
+        if(("/*-+(%").includes(equationElement.innerHTML.at(-1))) {
+            this.appendValue('(');
+            return;
+        }
+        
+        if(('0123456789.)').includes(equationElement.innerHTML.at(-1))) {
+            if(noOfRepetitions(equationElement.innerHTML, '(') > noOfRepetitions(equationElement.innerHTML, ')')) {
+                this.appendValue(')');
+            } else if((noOfRepetitions(equationElement.innerHTML, '(') == noOfRepetitions(equationElement.innerHTML, '('))) {
+                this.appendValue('(');
+            }
+        }
+    },
+
+    appendValue: function(value) {
+        if((equationElement.innerHTML !== '0')) {
+            equationElement.innerHTML += value;
+        } else {
+            equationElement.innerHTML = value;
+        }
+    },
+
+    backSpace: function() {
+        equationElement.innerHTML = equationElement.innerHTML.slice(0, -1);
+        if (equationElement.innerHTML === '') {
+            equationElement.innerHTML = "0";
+        }
+    },
+
+    zero: function() {
+        const operands = equationElement.innerHTML.split(/[+\-%/*()]/);
+        const lastOperand = operands.at(-1);
+
+        if((lastOperand).includes(".") || Number(lastOperand) > 0 || Number(lastOperand) < 0) {
+            this.appendValue(0);
+        }else if(lastOperand === '') {
+            this.appendValue(0);
+        }
+    },
+
+    point: function() {
+        if(("/*-+()%").includes(equationElement.innerHTML.at(-1))) {
+            this.appendValue('0.');
+            return;
+        }
+
+        const operands = equationElement.innerHTML.split(/[+\-%/*()]/);
+        const lastOperand = operands.at(-1);
+        if(!lastOperand.includes('.')) {
+            equationElement.innerHTML += '.';
+        }
+    },
+
+    clear: function() {
+        equationElement.innerHTML = '0';
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-import { noOfRepetitions } from "./utils/string-utils.js";
-
+import {operations} from './operations.js';
 
 
 const equationElement = document.querySelector('.js-equation');
@@ -9,25 +8,25 @@ addButtonClickListeners();
 document.addEventListener('keydown', (key) => {
     console.log(key.key);
     if(key.key === '0') {
-        zero();
+        operations.zero();
     }else if(Number.isInteger(Number(key.key))) {
-        appendValue(key.key);
+        operations.appendValue(key.key);
     }else if(key.key === 'Backspace') {
-        backSpace();
+        operations.backSpace();
     }else if(key.key === '.') {
-        point();
+        operations.point();
     }else if(key.key === "+") {
-        plus();
+        operations.plus();
     }else if(key.key === "-") {
-        minus();
+        operations.minus();
     }else if(key.key === "*") {
-        multiply();
+        operations.multiply();
     }else if(key.key === "/") {
-        divide();
+        operations.divide();
     }else if(key.key === "%") {
-        appendValue('%');
+        operations.appendValue('%');
     }else if((key.key === ")")  || (key.key === "(") ){
-        parenthesis();
+        operations.parenthesis();
     }
 });
 
@@ -35,158 +34,49 @@ document.addEventListener('keydown', (key) => {
 function addButtonClickListeners() {
 
     document.querySelector('.button-0').addEventListener('click', () => {
-        zero();
+        operations.zero();
     });
 
     for(let i = 1; i <= 9; i++) {
         const element = document.querySelector(`.button-${i}`);
         element.addEventListener('click', () => {
-            appendValue(i);
+            operations.appendValue(i);
         });
     }
 
     document.querySelector('.button-delete').addEventListener('click', () => {
-        backSpace();
+        operations.backSpace();
     });
 
     document.querySelector('.button-point').addEventListener('click', () => {
-        point();
+        operations.point();
     });
 
     document.querySelector('.button-ac').addEventListener('click', () => {
-        clear();
+        operations.clear();
     });
 
     document.querySelector('.button-plus').addEventListener('click', () => {
-        plus();
+        operations.plus();
     });
 
     document.querySelector('.button-minus').addEventListener('click', () => {
-        minus();
+        operations.minus();
     });
 
     document.querySelector('.button-parenthesis').addEventListener('click', () => {
-        parenthesis();
+        operations.parenthesis();
     });
 
     document.querySelector('.button-multiply').addEventListener('click', () => {
-        multiply();
+        operations.multiply();
     });
 
     document.querySelector('.button-divide').addEventListener('click', () => {
-        divide();
+        operations.divide();
     });
 
     document.querySelector('.button-percent').addEventListener('click', () => {
-        appendValue('%');
+        operations.appendValue('%');
     });
-}
-
-function appendValue(value) {
-    if((equationElement.innerHTML !== '0')) {
-        equationElement.innerHTML += value;
-    } else {
-        equationElement.innerHTML = value;
-    }
-}
-
-function backSpace() {
-    equationElement.innerHTML = equationElement.innerHTML.slice(0, -1);
-    if (equationElement.innerHTML === '') {
-        equationElement.innerHTML = "0";
-    }
-}
-
-function zero() {
-    const operands = equationElement.innerHTML.split(/[+\-%/*()]/);
-    const lastOperand = operands.at(-1);
-
-    // console.log(operands);
-    // console.log(lastOperand);
-
-    // console.log('(lastOperand).includes(".")');
-    // console.log((lastOperand).includes("."));
-
-
-    // console.log('Number(lastOperand) > 0');
-    // console.log(Number(lastOperand) > 0);
-
-
-    // console.log('Number(lastOperand) < 0');
-    // console.log(Number(lastOperand) < 0);
-
-
-    if((lastOperand).includes(".") || Number(lastOperand) > 0 || Number(lastOperand) < 0) {
-        appendValue(0);
-    }else if(lastOperand === '') {
-        appendValue(0);
-    }
-}
-
-function point() {
-    if(("/*-+()%").includes(equationElement.innerHTML.at(-1))) {
-        appendValue('0.');
-        return;
-    }
-
-    const operands = equationElement.innerHTML.split(/[+\-%/*()]/);
-    const lastOperand = operands.at(-1);
-    if(!lastOperand.includes('.')) {
-        equationElement.innerHTML += '.';
-    }
-}
-
-function clear() {
-    equationElement.innerHTML = '0';
-}
-
-function plus() {
-    if(!("/*-+%").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML += '+';
-    } else {
-        equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '+';
-    }
-}
-
-function minus() {
-    if(!("/*-+%").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML += '-';
-    } else {
-        equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '-';
-    }
-}
-
-function multiply() {
-    if((")%1234567890.").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML += '*';
-    } else if(equationElement.innerHTML.at(-1) === '(') {
-        return;
-    } else if (("/*-+").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '*';
-    }
-}
-
-function divide() {
-    if((")%1234567890.").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML += '/';
-    } else if(equationElement.innerHTML.at(-1) === '(') {
-        return;
-    } else if (("/*-+").includes(equationElement.innerHTML.at(-1))) {
-        equationElement.innerHTML = equationElement.innerHTML.slice(0, -1) + '/';
-    }
-}
-
-function parenthesis() {
-    if(("/*-+(%").includes(equationElement.innerHTML.at(-1))) {
-        appendValue('(');
-        return;
-    }
-    
-    if(('0123456789.)').includes(equationElement.innerHTML.at(-1))) {
-        if(noOfRepetitions(equationElement.innerHTML, '(') > noOfRepetitions(equationElement.innerHTML, ')')) {
-            appendValue(')');
-        } else if((noOfRepetitions(equationElement.innerHTML, '(') == noOfRepetitions(equationElement.innerHTML, '('))) {
-            appendValue('(');
-        }
-    }
 }
